b3e29cdfc6184f8c89741e9a734ff27f2a098bde
ZenKing <94725061+MrVenumX@users.noreply.github.com>
2022-10-24 10:10:31 +0700

    Module

---
+++ scoreboards.js
@@ -0,0 +1,39 @@
+/*Scoreboard Modules*/
+
+import { Dimension, world } from "mojang-minecraft";
+const dimension = world.getDimension("overworld");
+
+export function getScore(playerName, objectiveName) {
+  // get statusMessage
+  var score = dimension
+    .runCommand(`scoreboard players test ${playerName} ${objectiveName} * *`)
+    .statusMessage.split(" ");
+  return score[1];
+}
+
+export function setScore(playerName, objectiveName, Count) {
+  // set Score Players
+  try {
+    dimension.runCommand(
+      `scoreboard players set ${playerName} ${objectiveName} ${Count}`
+    );
+  } catch (error) {console.error(`setScore Error : ${error}`)}
+}
+
+export function addScore(playerName, objectiveName, Count) {
+  // add Score Players
+  try {
+    dimension.runCommand(
+      `scoreboard players add ${playerName} ${objectiveName} ${Count}`
+    );
+  } catch (error) {console.error(`setScore Error : ${error}`)}
+}
+
+export function removeScore(playerName, objectiveName, Count) {
+  // remove Score Players
+  try {
+    dimension.runCommand(
+      `scoreboard players remove ${playerName} ${objectiveName} ${Count}`
+    );
+  } catch (error) {console.error(`setScore Error : ${error}`)}
+}
